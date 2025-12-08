547d2297bdec381783142bd77a253773add0b21c
unknown <celso.sbj00@gmail.com>
2025-12-08 14:56:06 -0300

    Atribuição de Objeto via desestruturação

---
+++ Secao3/For/main.js
@@ -0,0 +1,11 @@
+// for (let i = 0; i <= 5; i++) {
+//     const par = i % 2 == 0 ? 'par' : 'impar'
+//     console.log(`Linha ${i}`, par);
+// }
+
+const frutas = ['maça', 'pera', 'uva'];
+for (x in frutas) {
+    console.log(frutas[x]);
+}
+
+
